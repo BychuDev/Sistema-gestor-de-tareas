9d0226e67ed4895a7eb4e8f74bcbd78f4f5af942
room4am0k <room4am0k@users.noreply.github.com>
2024-11-22 18:03:25 -0300

    Archivos de conexion

---
+++ modelo/config.php
@@ -0,0 +1,10 @@
+<?php
+$env = parse_ini_file(trim("../.htaccess/access.env"));  
+return [
+    'host' => $env['DB_HOST'] ?? 'localhost',
+    'dbname' => $env['DB_NAME'] ?? '',
+    'user' => $env['DB_USER'] ?? '',
+    'password' => $env['DB_PASSWORD'] ?? '',
+    'charset' => $env['DB_CHARSET'] ?? 'utf8'
+];
+?>
